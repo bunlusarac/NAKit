64899c1ed549677cca46909a4437161713686a4b
bunlusarac <7127523+bunlusarac@users.noreply.github.com>
2025-04-26 21:40:37 +0300

    Test project and dynamic library project

---
+++ NAKit.Core/include/NAKit.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef _WIN32
+    #ifdef NAKIT_EXPORTS
+        #define LIB_API __declspec(dllexport)
+    #else 
+        #define LIB_API __declspec(dllimport)
+    #endif
+#else
+    #define LIB_API
+#endif
+
+LIB_API double Square(double);
---
+++ NAKit.Core/src/Square.cpp
@@ -0,0 +1,5 @@
+#include "NAKit.h"
+
+double Square(double x) {
+    return x * x;
+}
